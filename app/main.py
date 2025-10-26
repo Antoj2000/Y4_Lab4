a48181544af3183ee6d8ffc87ee51da861e43ad9
Antoj2000 <g00385306@atu.ie>
2025-10-26 01:42:47 +0000

    Added courseDB and projectDB along with APIs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 # app/main.py
 
-from fastapi import FastAPI, HTTPException, status, Depends
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, HTTPException, status, Depends, Response
+from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from .database import SessionLocal, engine
-from .schemas import UserCreate, UserRead
-from .models import Base, UserDB
+from .schemas import (
+    UserCreate, UserRead,
+    CourseCreate, CourseRead,
+    ProjectCreate, ProjectRead,
+    ProjectReadWithOwner, ProjectCreateForUser
+)
+from .models import Base, UserDB, CourseDB, ProjectDB
 
 app = FastAPI()
 # Uncomment this line to reset DB
@@ -21,6 +26,100 @@ def get_db():
     finally:
         db.close()
 
+
+def commit_or_rollback(db: Session, error_msg: str):
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback
+
+@app.get("/health")
+def health():
+    return {"status" : "ok"}
+
+# --- Courses ---
+# Add a course
+@app.post("/api/courses", response_model=CourseRead, status_code=201, summary="You could add details")
+def create_course(course: CourseCreate, db: Session = Depends(get_db)):
+    db_course = CourseDB(**course.model_dump())
+    db.add(db_course)
+    commit_or_rollback(db, "Course already exists")
+    db.refresh(db_course)
+    return db_course
+
+# List all courses
+@app.get("/api/courses", response_model=list[CourseRead])
+def list_courses(limit: int = 10, offset: int = 0, db: Session = Depends(get_db)):
+    stmt = select(CourseDB).order_by(CourseDB.id).limit(limit).offset(offset)
+    return db.execute(stmt).scalars().all()
+
+
+# --- Projects ---
+# Create a project
+@app.post("/api/projects", response_model=ProjectRead, status_code=201)
+def create_project(project: ProjectCreate, db: Session = Depends(get_db)):
+    user = db.get(UserDB, project.owner_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    proj = ProjectDB(
+        name=project.name,
+        description=project.description,
+        owner_id=project.owner_id,
+    )
+    db.add(proj)
+    commit_or_rollback(db, "Project creation failed")
+    db.refresh(proj)
+    return proj
+
+# List all projects
+@app.get("/api/projects", response_model=list[ProjectRead])
+def list_projects(db: Session = Depends(get_db)):
+    stmt = select(ProjectDB).order_by(ProjectDB.id)
+    return db.execute(stmt).scalars().all()
+
+
+# Get projects by projectID
+@app.get("/api/projects/{project_id}", response_model=ProjectReadWithOwner)
+def get_project_with_owner(project_id: int, db: Session = Depends(get_db)):
+    stmt = select(ProjectDB).where(ProjectDB.id ==
+                                   project_id).options(selectinload(ProjectDB.owner))
+    proj = db.execute(stmt).scalar_one_or_none()
+    if not proj:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return proj
+
+
+# --- Nested Routes --- 
+
+# Get users with their related projects
+@app.get("/api/users/{users_id}/projects", response_model=list[ProjectRead])
+def get_user_projects(users_id: int, db: Session = Depends(get_db)):
+    stmt = select(ProjectDB).where(ProjectDB.owner_id == users_id)
+    #space it out for debugging
+    result = db.execute(stmt)
+    rows = result.scalars().all()
+    return rows
+    #return db.execute(stmt).scalars().all()
+
+# Add a project to a user
+@app.post("/api/users/{user_id}/projects", response_model=ProjectRead, status_code=201)
+def create_user_project(users_id: int, project: ProjectCreateForUser, db: Session = Depends(get_db)):
+    user = db.get(UserDB, users_id)
+    if not user: 
+        raise HTTPException(status_code=404, detail="User not found")
+    proj = ProjectDB(
+        name=project.name,
+        description=project.description,
+        owner_id=users_id
+    )
+    db.add(proj)
+    commit_or_rollback(db, "Project creation failed")
+    db.refresh(proj)
+    return proj
+
+# --- Users
+
 #Get all users
 @app.get("/api/users", response_model=list[UserRead])
 def list_users(db: Session = Depends(get_db)):
@@ -30,9 +129,9 @@ def list_users(db: Session = Depends(get_db)):
     return list(db.execute(stmt).scalars())
 
 #Get user by id
-@app.get("/api/users/{user_id}", response_model=UserRead)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.get(UserDB, user_id)
+@app.get("/api/users/{users_id}", response_model=UserRead)
+def get_user(users_id: int, db: Session = Depends(get_db)):
+    user = db.get(UserDB, users_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
@@ -52,9 +151,9 @@ def add_user(payload: UserCreate, db: Session = Depends(get_db)):
     return user
 
 # Update User
-@app.put("/api/users/update/{user_id}", response_model=UserRead)
-def update_user(user_id: int, payload: UserCreate, db: Session = Depends(get_db)):
-    user = db.get(UserDB, user_id)
+@app.put("/api/users/update/{users_id}", response_model=UserRead)
+def update_user(users_id: int, payload: UserCreate, db: Session = Depends(get_db)):
+    user = db.get(UserDB, users_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     for key, value in payload.model_dump().items():
@@ -69,9 +168,9 @@ def update_user(user_id: int, payload: UserCreate, db: Session = Depends(get_db)
     
 
 #Delete User
-@app.delete("/api/users/delete/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.get(UserDB, user_id)
+@app.delete("/api/users/delete/{users_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(users_id: int, db: Session = Depends(get_db)):
+    user = db.get(UserDB, users_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     db.delete(user)
